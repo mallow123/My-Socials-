47e1f9b0ce4c6267d99a0a00565de01ac214ab4c
Bazerk <126198264+mallow123@users.noreply.github.com>
2024-10-22 02:42:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for additional animations, interactivity, or any future functionality you want to add.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page loaded and ready!');
+});
